0714a92d65c3f82d2175ed8e7ca18044682b512b
wasshoidokkoishoi <otumamiumeee+sub@gmail.com>
2024-11-18 01:50:45 +0900

    Update tekito.js

--- tekito.js
+++ tekito.js
@@ -1,4 +1,4 @@
-//document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("load", function() {
   const links = document.querySelectorAll('a[href^="#"]');
   links.forEach(function(link) {
     link.addEventListener("click", function(event) {
@@ -35,7 +35,7 @@
       topBtn.style.display = "none";
     }
   });
-//});
+});
 
 /* nav にクラスを付与するscript */
 window.addEventListener("scroll", function() {
